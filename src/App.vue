8f3da4c4c8090510b8dffe4df082526d4f95a7be
wangxin <wangxin@wx.com>
2018-09-13 09:24:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header class="header">
+        <vheader></vheader>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <nav_menu></nav_menu>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+import NavMenu from '@/components/NavMenu'
+import Header from '@/components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    'nav_menu': NavMenu,
+    'vheader': Header
+  }
+}
+</script>
+
+<style>
+  .header {
+    background-color: #409EFF;
+    color: #fff;
+    line-height: 60px;
+  }
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-row>
+    <el-col :span="24">
+    <div class="head-wrap"><i class="el-icon-picture"></i>Element22</div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  .head-wrap{
+
+  }
+</style>
